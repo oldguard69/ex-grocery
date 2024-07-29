b267d02b8ffa2d3ae7fe42d8c59f196de800884d
Thuan Tran <tvthuan1409@gmail.com>
2024-07-29 21:20:33 +0700

    Add CORS policy

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from app.common import models, schemas
@@ -17,6 +18,21 @@ models.Base.metadata.create_all(engine)
 
 
 app = FastAPI()
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/register")
